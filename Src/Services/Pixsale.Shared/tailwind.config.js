2879851817f2d28da29ee1b798a9610bc2176e7a
Roy Subrata <roy.subrata.cs@gmail.com>
2025-04-20 09:56:07 +0600

    feat: setup tailwind css

---
+++ Src/Services/Pixsale.Shared/tailwind.config.js
@@ -0,0 +1,14 @@
+﻿/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        //"./Pages/**/*.{razor,cshtml}",
+        //"./Layout/**/*.{razor,cshtml}",
+        //"./Components/**/*.{razor,cshtml}",
+        //"./wwwroot/**/*.{html,js}",
+        //"../Pixsale.Web/Components/**/*.{razor,cshtml}",
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
