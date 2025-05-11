94c78a738ab2e1c2e66ea0a9e056472a4b106138
Roy Subrata <roy.subrata.cs@gmail.com>
2025-05-11 12:55:46 +0600

    feat:added unit api feature

---
+++ Src/Services/Pixsale.Application/Models/Unit.cs
@@ -0,0 +1,4 @@
+﻿namespace Pixsale.Application.Models;
+public record CreateUnit(string Name, string ShortName, decimal ConversionFactor);
+public record UpdateUnit(Guid Id, string Name, string ShortName, decimal ConversionFactor);
+public record GetUnit(Guid Id, string Name, string ShortName, decimal ConversionFactor);
---
+++ Src/Services/Pixsale.Shared/Clients/Models/Unit.cs
@@ -0,0 +1,7 @@
+﻿namespace Pixsale.Shared.Clients.Models;
+public record Unit(
+    Guid Id,
+    string Name,
+    string ShortName,
+    decimal ConversionFactor
+);
